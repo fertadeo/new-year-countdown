f0dcbcef8b560c16f205e3ddae1b359fbf8b5a5f
Fernando Tadeo <fernandotadeos@gmail.com>
2024-01-16 14:33:20 -0300

    se arregla el contador para los años siguientes

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
-const secondsEl  = document.getElementById('seconds');
+const secondsEl = document.getElementById('seconds');
+const yearTitleEl = document.getElementById('yearTitle');
 
+function setNextYearTitle() {
+  const currentYear = new Date().getFullYear();
+  const nextYear = currentYear + 1;
+  yearTitleEl.innerHTML = `New Year ${nextYear}`;
+}
 
+setNextYearTitle()
 
-const newYears = "1 Jan 2023";
+// Obtén la fecha actual y el próximo Año Nuevo
+const currentDate = new Date();
+let newYearsDate = new Date(currentDate.getFullYear() + 1, 0, 1);
 
-function countdown() {
-  const newYearsDate = new Date(newYears);
+function updateCounter() {
   const currentDate = new Date();
+  let newYearsDate = new Date(currentDate.getFullYear() + 1, 0, 1);
 
-  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
 
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 24 % 60; 
+  const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
-  
-
 
   daysEl.innerHTML = days;
   hoursEl.innerHTML = formatTime(hours);
@@ -30,8 +38,8 @@ function formatTime(time) {
   return time < 10 ? (`0${time}`) : time;
 }
 
-//Initial call //
+// Llama a la función countdown cada segundo
+setInterval(updateCounter, 1000);
 
-setInterval(countdown, 1000);
-
-countdown();
+// Llama a countdown para la inicialización
+updateCounter();
